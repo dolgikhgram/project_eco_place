ad2f83c5c135f89374242f74faa7b5cef56d86c7
Nikita Dolgikh <n.dolgik.020@mail.ru>
2024-05-04 12:23:16 +0300

    first commit

---
+++ src/Header/Header.js
@@ -0,0 +1,18 @@
+
+
+const Header = ()=>{
+    return(
+        <div>
+            <ul>
+                <li>
+                    css
+                </li>
+                <li>js</li>
+                <li>react</li>
+                <li>html</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header
